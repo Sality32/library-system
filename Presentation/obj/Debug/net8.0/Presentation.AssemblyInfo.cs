138555c1504b9b8149b28ee9a6e127141556ff01
Yoga Subagja <42853517+Sality32@users.noreply.github.com>
2025-04-06 19:56:30 +0700

    add asembly update information

--- Presentation/obj/Debug/net8.0/Presentation.AssemblyInfo.cs
+++ Presentation/obj/Debug/net8.0/Presentation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ed9fe1dca27300916cdf2499063a8ac5b55792c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15122ececc9e7c2d9781fcc69e8e7b2d925f990c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
